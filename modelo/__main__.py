eecd47cd14b6d6c8e89225f1a8ba40db19b12938
Rafhael Martins <rafha-1012@hotmail.com>
2021-09-03 15:42:40 -0300

    :sparkles: add: new function

--- modelo/__main__.py
+++ modelo/__main__.py
@@ -11,19 +11,19 @@ class Model:
         self.df_cr = pd.read_csv("../dados/credit_record.csv")
         
     def PreparingData(self):
-        #print(pre_processing.PreProcessing.geraClasses(0, 525000, 25000, np.Infinity))
-        classes_renda = Model.generates_classes(0, 525000, 25000)
-        #print(classes_renda)
-        return(self, classes_renda)
+        classes_renda = pre_processing.PreProcessing.geraClasses(0, 525000, 25000, np.Infinity)
+        #classes_renda = PreProcessing.geraClasses(0, 525000, 25000)
+        print(classes_renda)
+        #return(self, classes_renda)
         
-    # Função para gerar valores de classes para o cut.
+"""    # Função para gerar valores de classes para o cut.
     def generates_classes(inicio, fim, intervalo):
         adiciona = np.Infinity
         classes_faixas = []
         classes_faixas = list(range(inicio, fim, intervalo))
         classes_faixas.append(adiciona)
-        return classes_faixas
+        return classes_faixas"""
         
 
-#Model.PreparingData(Model())
-Model.generates_classes(Model(0, 525000, 25000))
+Model.PreparingData(Model())
+#Model.generates_classes(Model(0, 525000, 25000))
--- modelo/pre_processing.py
+++ modelo/pre_processing.py
@@ -12,8 +12,52 @@ class PreProcessing:
       #classes_renda.append(1000000)
       classes_faixas.append(adiciona)
       #print("Classes: ", classes)
-    return classes_faixas
+      return classes_faixas
   
+    
+'''   
+    # Função para gerar labels das classes para o cut.
+    def geraFaixasClasse(classe):
+      # Labels para as faixas de renda.
+      labels_faixas = classe.copy()
+      labels_faixas.remove(0)
+      #print("Labels Faixas: ", labels_faixas)
+      #return classes, labels_faixas
+      #rangeForText(classe, labels_faixas)
+      return labels_faixas
+      
+    # Função para gerar labels das classes para gráficos.
+    def rangeForTextInGraph(classe, labels_faixas):
+      faixas = []
+      contador = 0
+    
+      for i, ii in zip(classe, labels_faixas):
+        if contador < 1:
+          faixas.append("{} a {}".format(i, ii))
+          contador += 1
+        else:
+          faixas.append("{} a {}".format(i+1, ii))
+    
+      return faixas
+  
+    # Função para gerar valor da faixa no DataFrame.
+    def FaixasAtTheData(dados, coluna:str, classes, faixas):
+      dados[coluna + "_BIN"] = pd.cut(x = dados[coluna], 
+                                      bins = classes, 
+                                      labels = faixas)
+      return dados[coluna + "_BIN"]
+    
+    # Função para gerar faixa no DataFrame para plotar no gráfico.
+    def FaixasForGraphAtTheData(dados, coluna:str, classes, faixas):
+      dados[coluna + "_TEXT"] = pd.cut(x = dados[coluna], 
+                                      bins = classes, 
+                                      labels = faixas)
+      return dados[coluna + "_TEXT"]
+    
+    # Função para transformar valor em string.
+    def rangeForText(dados, coluna):
+      dados[coluna+"_TEXT_TESTE"] = dados[coluna].apply(lambda x:str(x))
+      return dados[coluna+"_TEXT_TESTE"] '''
     
 '''   
     # Função para gerar labels das classes para o cut.
