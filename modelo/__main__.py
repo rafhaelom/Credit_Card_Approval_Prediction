26b85fc9e8c07c07e63559742140aa177d7046cd
Rafhael Martins <rafha-1012@hotmail.com>
2021-09-03 12:39:52 -0300

    :tada: add: new script

--- modelo/__main__.py
+++ modelo/__main__.py
@@ -1,2 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import pandas as pd
+
+class Model():
+    def __init__(self):
+        self.df_ap = pd.read_csv("./dados/application_record.csv")
+        self.df_cr = pd.read_csv("C:/Users/rafhael.martins/Documents/Aulas_Thiago-Russo/Práticas/Credit_Card_Approval_Prediction1/Credit_Card_Approval_Prediction/dados/credit_record.csv")
+        
+    def read_files(self):
+        print(self.df_ap.head())
+        print(self.df_ap.info(memory_usage='deep'))
+        
+
+Model.read_files(Model())
